8ba5d5027f3c3e806ccc45c0a62ca947d5c2d12b
mnk-19900910 <wccf19900910@yahoo.co.jp>
2021-05-15 15:16:29 +0900

    ログアウト機能の追加

--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -11,5 +11,7 @@
     <a href="../staff/staff_list.php">スタッフ管理</a><br>
     <br>
     <a href="../product/pro_list.php">商品管理</a><br>
+    <br>
+    <a href="staff_logout.php">ログアウト</a><br>
 </body>
 </html>
