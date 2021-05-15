76808faaaddb48dfebec7d9e11e02960482515e3
mnk-19900910 <wccf19900910@yahoo.co.jp>
2021-05-15 15:35:47 +0900

    連想配列の練習

---
+++ asobi/hoshi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>1995</title>
+</head>
+<body>
+    <?php
+        $mbango=$_POST['mbango'];
+        $hoshi['M1']='カニ星雲';
+        $hoshi['M31']='アンドロメダ大星雲';
+        $hoshi['M42']='オリオン大星雲';
+        $hoshi['M45']='昴';
+        $hoshi['M57']='ドーナツ星雲';
+        print 'あなたが選んだ星は、';
+        print $hoshi[$mbango];
+        print 'です';
+    ?>
+</body>
+</html>
