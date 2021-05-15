a4cf61130ea7bdd9dff52fca072ebd5d57072d67
mnk-19900910 <wccf19900910@yahoo.co.jp>
2021-05-15 17:13:17 +0900

    関数作りの練習

---
+++ common/common.php
@@ -0,0 +1,10 @@
+<?php
+    function gengo($seireki){
+        if($seireki>=1868 && $seireki<=1911) $gengo='明治';
+        if($seireki>=1912 && $seireki<=1925) $gengo='大正';
+        if($seireki>=1926 && $seireki<=1988) $gengo='昭和';
+        if($seireki>=1989 && $seireki<=2018) $gengo='平成';
+        if($seireki>=2019) $gengo='令和';
+        return($gengo);
+    }
+?>
