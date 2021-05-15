9eb3719a3f77935a862a340779d178056487c2a8
mnk-19900910 <wccf19900910@yahoo.co.jp>
2021-05-15 15:35:39 +0900

    配列の練習

---
+++ asobi/shun.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>1995</title>
+</head>
+<body>
+    <?php
+        $tsuki=$_POST['tsuki'];
+        $yasai[]='';
+        $yasai[]='ブロッコリー';
+        $yasai[]='カリフラワー';
+        $yasai[]='レタス';
+        $yasai[]='みつば';
+        $yasai[]='アスパラガス';
+        $yasai[]='セロリ';
+        $yasai[]='ナス';
+        $yasai[]='ピーマン';
+        $yasai[]='オクラ';
+        $yasai[]='さつまいも';
+        $yasai[]='大根';
+        $yasai[]='ほうれん草';
+        print $tsuki;
+        print '月は';
+        print $yasai[$tsuki];
+        print 'が旬です';
+    ?>
+</body>
+</html>
