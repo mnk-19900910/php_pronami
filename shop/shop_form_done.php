14c39bf3a5b891b4372da60d4b3b58e06dd6fc87
mnk-19900910 <wccf19900910@yahoo.co.jp>
2021-05-19 16:12:48 +0900

    注文データを追加するSQLの追加

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -45,6 +45,7 @@
             $dbh=new PDO($dsn,$user,$password);
             $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
+            // mst_product表に商品情報を保存する
             for($i=0;$i<$max;$i++){
                 $sql='SELECT name,price FROM mst_product WHERE code=?';
                 $stmt=$dbh->prepare($sql);
@@ -53,13 +54,45 @@
                 $rec=$stmt->fetch(PDO::FETCH_ASSOC);
                 $name=$rec['name'];
                 $price=$rec['price'];
+                $kakaku[]=$price;
                 $suryo=$kazu[$i];
                 $shokei=$price*$suryo;
-
                 $honbun.=$name.' '.$price.'円 x '.$suryo.'個 = '.$shokei."円 \n";
+            }
+
+            // dat_sales表に注文データを保存する
+            $sql='INSERT INTO dat_sales(code_member,name,email,postal1,postal2,address,tel) VALUES(?,?,?,?,?,?,?)';
+            $stmt=$dbh->prepare($sql);
+            $data=array();
+            $data[]=0;
+            $data[]=$onamae;
+            $data[]=$email;
+            $data[]=$postal1;
+            $data[]=$postal2;
+            $data[]=$address;
+            $data[]=$tel;
+            $stmt->execute($data);
+
+            // dat_sales_product表に商品詳細を追加する
+            $sql='SELECT LAST_INSERT_ID()'; //AUTO_INCREMENTで直近に発番された番号を取得
+            $stmt=$dbh->prepare($sql);
+            $stmt->execute($data);
+            $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+            $lastcode=$rec['LAST_INSERT_ID()']; //取得した注文コードを$lastcodeに格納
+
+            for($i=0;$i<$max;$i++){
+                $sql='INSERT INTO dat_sales_product(code_sales,code_product,price,quantity) VALUES(?,?,?,?)';
+                $stmt=$dbh->prepare($sql);
+                $data=array();
+                $data[]=$lastcode;
+                $data[]=$cart[$i];
+                $data[]=$kakaku[$i];
+                $data[]=$kazu[$i];
+                $stmt->execute($data);
             }
             $dbh=null;
 
+            // お客様へのメール本文
             $honbun.="送料は無料です。\n";
             $honbun.="----------------------\n\n";
             $honbun.="代金は以下の口座にお振込みください。\n";
@@ -72,10 +105,11 @@
             $honbun.="メール info@mattuacademy.co.jp\n";
             $honbun.="**************************\n";
 
+            // メール内容をブラウザで確認
             // print '<br>';
             // print nl2br($honbun);
 
-            // お客様へのメール
+            // お客様へのメール送信
             $title=$onamae.'様　ご注文ありがとうございます。';
             $header='From:info@mattuacademy.co.jp';
             $honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
@@ -83,7 +117,7 @@
             mb_internal_encoding('UTF-8');
             mb_send_mail($email,$title,$honbun,$header);
 
-            // お店へのメール
+            // お店へのメール送信
             $title='お客様からご注文がありました。';
             $header='From:'.$email;
             $honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
